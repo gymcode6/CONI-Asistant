45a8780b8f86cfd40723545a8500d3435af2d060
Quiqueoso <quiqueoso.63@gmail.com>
2024-04-14 15:56:13 -0600

    (srry) REcreated backend (-_-)

---
+++ coni-logic/coniasistant/src/test/java/com/coni/backend/asistant/coniasistant/ConiasistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coni.backend.asistant.coniasistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConiasistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
